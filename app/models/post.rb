04198c47eaf35a9d9528eec863eff8607e1b29f5
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-12-08 19:23:20 +0000

    Add crud methods to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :author, class_name: 'User'
+
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.size)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
-  has_many :posts, foreign_key:  'author_id'
+  has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
 
   def select_top_three
