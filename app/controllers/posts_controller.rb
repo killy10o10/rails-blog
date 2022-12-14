3267f79b3f7cb8953df0e178d19961f6108aa392
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-12-14 20:31:35 +0000

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @index_message = "This is a page to all posts"
+    @index_message = 'This is a page to all posts'
   end
 
   def show
-    @show_message = "This is a page for a specific post"
+    @show_message = 'This is a page for a specific post'
   end
 end
