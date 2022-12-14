9f7ded2b838266d7c27f2942b966ed1493ec88db
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-12-14 20:22:48 +0000

    Add controllers and routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+  def index
+    @index_message = "This is a page to all posts"
+  end
+
+  def show
+    @show_message = "This is a page for a specific post"
+  end
+end
