e03a8d2a7bca39d69ebeec55adad5b5115aa7689
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-12-21 01:30:39 +0000

    Fix n+1 problems
    Add spec for users index

---
+++ spec/integrations/users/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'user/#index', type: :feature do
+  before :each do
+    visit users_path
+    @users = User.all
+  end
+  describe 'index page' do
+    it 'should show the names of all users' do
+      @users.each { |user| expect(page).to have_content(user.name) }
+    end
+
+    it 'should show the profile picture of each user' do
+      @users.each { |user| expect(page).to have_css("img[src*='#{user.photo}']") }
+    end
+
+    it 'should show the number of posts of each user' do
+      @users.each { |user| expect(page).to have_content "Number of posts: #{user.posts_counter}" }
+    end
+
+    # it 'should redirect user to show page when a user is clicked' do
+    #   @users.each { |user| expect(user).to link_to user_path(user.id) }
+    # end
+  end
+end
